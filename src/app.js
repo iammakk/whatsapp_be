b69fdc57ab5b96cafa496230369f89ac208fc8a1
azeem <stepsink@gmail.com>
2023-06-24 03:11:26 -0700

    setup creoss-env

--- src/app.js
+++ src/app.js
@@ -1,7 +1,37 @@
 import express from "express"
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import ExpressMongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
 
+dotenv.config();
 
 const app = express();
+if(process.env.NODE_ENV !== "production"){
+
+    app.use(morgan("dev"));
+}
+app.use(helmet());
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(ExpressMongoSanitize())
+app.use(cookieParser())
+app.use(compression())
+app.use(fileUpload({
+    useTempFiles:true,
+
+}))
+app.use(cors())
+
+
+
+
+
+
 
 
 
